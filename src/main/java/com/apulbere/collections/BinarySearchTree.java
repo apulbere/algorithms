003cc90d88c7e295826bc103322106a863915b34
apulbere <apulbere@yahoo.com>
2019-01-03 19:02:39 +0200

    BST

--- src/main/java/com/apulbere/collections/BinarySearchTree.java
+++ src/main/java/com/apulbere/collections/BinarySearchTree.java
@@ -5,6 +5,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.*;
+import java.util.function.Consumer;
+
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,20 +15,94 @@ import lombok.ToString;
 public class BinarySearchTree<T extends Comparable<T>> {
     private Node<T> root;
 
-    public void add(T value) {
-        root = addRecursiveIf(value, root);
+    public BinarySearchTree(Collection<T> collection) {
+        collection.forEach(this::add);
     }
 
-    private Node<T> addRecursiveIf(T value, Node<T> node) {
+    public void add(T value) {
+        Objects.requireNonNull(value, "null value not allowed");
+        root = addRecursive(value, root);
+    }
+
+    private Node<T> addRecursive(T value, Node<T> node) {
         if(node == null) {
             return new Node<>(value);
         }
-        if(value.compareTo(node.value) > 0) {
-            node.left = addRecursiveIf(value, node.left);
+        if(value.compareTo(node.value) < 0) {
+            node.left = addRecursive(value, node.left);
         } else {
-            node.right = addRecursiveIf(value, node.right);
+            node.right = addRecursive(value, node.right);
         }
         return node;
+    }
+
+    public boolean contains(T value) {
+        Objects.requireNonNull(value, "null value not allowed");
+        return findRecursive(value, root) != null;
+    }
+
+    private Node<T> findRecursive(T value, Node<T> node) {
+        if(node == null) {
+            return null;
+        }
+        int res = value.compareTo(node.value);
+        if(res == 0) {
+            return node;
+        }
+        return res < 0 ? findRecursive(value, node.left) : findRecursive(value, node.right);
+    }
+
+    /**
+     * Left, Root, Right
+     */
+    public List<T> inorder() {
+        return new Traversal<T>().left().root().right().traverse(root);
+    }
+
+    /**
+     * Root, Left, Right
+     */
+    public List<T> preorder() {
+        return new Traversal<T>().root().left().right().traverse(root);
+    }
+
+    /**
+     * Left, Right, Root
+     */
+    public List<T> postorder() {
+        return new Traversal<T>().left().right().root().traverse(root);
+    }
+
+    private static class Traversal<T> {
+        List<T> list = new LinkedList<>();
+        List<Consumer<Node<T>>> operations = new ArrayList<>(3);
+
+        private void traverseRecursive(Node<T> node) {
+            if(node == null) {
+                return;
+            }
+            operations.forEach(op -> op.accept(node));
+        }
+
+        Traversal<T> left() {
+            operations.add(node -> traverseRecursive(node.left));
+            return this;
+        }
+
+        Traversal<T> right() {
+            operations.add(node -> traverseRecursive(node.right));
+            return this;
+        }
+
+        Traversal<T> root() {
+            operations.add(node -> list.add(node.value));
+            return this;
+        }
+
+        List<T> traverse(Node<T> node) {
+            traverseRecursive(node);
+            return list;
+        }
     }
 
     @ToString
--- src/test/java/com/apulbere/collections/BinarySearchTreeTest.java
+++ src/test/java/com/apulbere/collections/BinarySearchTreeTest.java
@@ -3,7 +3,9 @@ package com.apulbere.collections;
 import com.apulbere.collections.BinarySearchTree.Node;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BinarySearchTreeTest {
 
@@ -16,10 +18,33 @@ public class BinarySearchTreeTest {
         bst.add(5);
         bst.add(1);
 
-        var node6 = new Node<>(new Node<>(23), new Node<>(5), 6);
+        var node6 = new Node<>(new Node<>(5), new Node<>(23), 6);
         var node1 = new Node<>(1);
-        var rootNode = new Node<>(node6, node1, 4);
+        var rootNode = new Node<>(node1, node6, 4);
         var expectedBst = new BinarySearchTree<>(rootNode);
         assertEquals(expectedBst, bst);
+    }
+
+    @Test
+    void contains() {
+        var values = List.of(5, 8, 9, 1, 0);
+        var bst = new BinarySearchTree<>(values);
+
+        assertTrue(values.stream().allMatch(bst::contains));
+        assertFalse(bst.contains(89));
+    }
+
+    @Test
+    void traverse() {
+        var bst = new BinarySearchTree<>(List.of(5, 15, 4, 10, 27, 3));
+
+        var inorder = List.of(3, 4, 5, 10, 15, 27);
+        assertEquals(inorder, bst.inorder());
+
+        var preorder = List.of(5, 4, 3, 15, 10, 27);
+        assertEquals(preorder, bst.preorder());
+
+        var postOrder = List.of(3, 4, 10, 27, 15, 5);
+        assertEquals(postOrder, bst.postorder());
     }
 }
