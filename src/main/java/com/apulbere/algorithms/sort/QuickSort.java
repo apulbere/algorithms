266a2b74704d8bdea9a7456e18cfb4098cc67423
apulbere <apulbere@yahoo.com>
2019-01-14 22:12:05 +0200

    fix

--- src/main/java/com/apulbere/algorithms/sort/QuickSort.java
+++ src/main/java/com/apulbere/algorithms/sort/QuickSort.java
@@ -7,7 +7,7 @@ public class QuickSort<T extends Comparable<T>> implements Sort<T> {
 
     /**
      *
-     * STEP 1: ick an element, called a pivot, from the array.
+     * STEP 1: Pick an element, called a pivot, from the array.
      * STEP 2: Partitioning: reorder the array so that all elements with values less than the pivot come before the pivot, while all elements with values greater
      *         than the pivot come after it (equal values can go either way). After this partitioning, the pivot is in its final position.
      * STEP 3: Recursively apply the above steps to the sub-array of elements with smaller values and separately to the sub-array of elements with greater values.
