f8d5a5c5e37bed9ab827ffcc2cf09577e4a1c88d
apulbere <apulbere@yahoo.com>
2019-02-04 20:26:12 +0200

    add shortest path with BFS

--- src/main/java/com/apulbere/algorithms/graph/AdjacencyListGraph.java
+++ src/main/java/com/apulbere/algorithms/graph/AdjacencyListGraph.java
@@ -1,8 +1,11 @@
 package com.apulbere.algorithms.graph;
 
 import lombok.Getter;
+import lombok.Value;
 
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 
@@ -10,9 +13,10 @@ public class AdjacencyListGraph<V> {
     @Getter
     private Map<V, List<V>> adjacencyList = new HashMap<>();
 
-    public void addEdge(V source, V destination) {
+    public AdjacencyListGraph<V> addEdge(V source, V destination) {
         adjacencyList.computeIfAbsent(source, k -> new ArrayList<>()).add(destination);
         adjacencyList.computeIfAbsent(destination, k -> new ArrayList<>()).add(source);
+        return this;
     }
 
     public Collection<V> depthFirstTraversal(V startVertex) {
@@ -39,5 +43,53 @@ public class AdjacencyListGraph<V> {
             adjacencyList.getOrDefault(vertex, emptyList()).stream().filter(result::add).forEach(stack::add);
         }
         return result;
+    }
+
+    /**
+     * time complexity O(V + E), where V is the number of vertices, and E is the number of Edges
+     * it means, whichever term is bigger will dominate the time complexity
+     */
+    public Path<V> shortestPathBFS(V source, V destination) {
+        var visited = new HashSet<V>();
+        var stack = new LinkedList<V>();
+        var predecessors = new HashMap<V, V>();
+        var distances = new HashMap<V, Integer>();
+        stack.push(source);
+        visited.add(source);
+        while(!stack.isEmpty()) {
+            V vertex = stack.pop();
+            for(V nextVertex: adjacencyList.getOrDefault(vertex, emptyList())) {
+                if(visited.add(nextVertex)) {
+                    predecessors.put(nextVertex, vertex);
+                    distances.put(nextVertex, distances.getOrDefault(vertex, 0) + 1);
+                    stack.add(nextVertex);
+                }
+                if(vertex.equals(destination)) {
+                    break;
+                }
+            }
+        }
+
+        if(predecessors.get(destination) == null) {
+            return Path.EMPTY;
+        }
+
+        var pathList = Stream.iterate(destination, Objects::nonNull, predecessors::get)
+                .collect(addAlwaysFirstCollector());
+        return new Path<>(distances.get(destination), pathList);
+    }
+
+    private Collector<V, LinkedList<V>, LinkedList<V>> addAlwaysFirstCollector() {
+        return Collector.of(LinkedList::new,
+                LinkedList::addFirst,
+                (a, b) -> { throw new UnsupportedOperationException("parallel processing not supported"); });
+    }
+
+    @Value
+    public static class Path<V> {
+        private int distance;
+        private Collection<V> path;
+
+        public static final Path EMPTY = new Path<>(0, emptyList());
     }
 }
--- src/test/java/com/apulbere/algorithms/graph/AdjacencyListGraphTest.java
+++ src/test/java/com/apulbere/algorithms/graph/AdjacencyListGraphTest.java
@@ -1,5 +1,6 @@
 package com.apulbere.algorithms.graph;
 
+import com.apulbere.algorithms.graph.AdjacencyListGraph.Path;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 class AdjacencyListGraphTest {
@@ -21,12 +23,12 @@ class AdjacencyListGraphTest {
                 5, List.of(2, 3, 4)
         );
 
-        var graph = new AdjacencyListGraph<Integer>();
-        graph.addEdge(1, 4);
-        graph.addEdge(4, 2);
-        graph.addEdge(4, 5);
-        graph.addEdge(5, 2);
-        graph.addEdge(3, 5);
+        var graph = new AdjacencyListGraph<Integer>()
+            .addEdge(1, 4)
+            .addEdge(4, 2)
+            .addEdge(4, 5)
+            .addEdge(5, 2)
+            .addEdge(3, 5);
         var graphAdjacencyList = graph.getAdjacencyList();
 
         expectedAdjacencyList.forEach((k, v) -> assertThat(v, containsInAnyOrder(graphAdjacencyList.get(k).toArray())));
@@ -37,16 +39,16 @@ class AdjacencyListGraphTest {
      */
     @Test
     void depthTraversal() {
-        var graph = new AdjacencyListGraph<String>();
-        graph.addEdge("S", "A");
-        graph.addEdge("S", "B");
-        graph.addEdge("S", "C");
-        graph.addEdge("A", "D");
-        graph.addEdge("D", "G");
-        graph.addEdge("B", "E");
-        graph.addEdge("E", "G");
-        graph.addEdge("C", "F");
-        graph.addEdge("F", "G");
+        var graph = new AdjacencyListGraph<String>()
+            .addEdge("S", "A")
+            .addEdge("S", "B")
+            .addEdge("S", "C")
+            .addEdge("A", "D")
+            .addEdge("D", "G")
+            .addEdge("B", "E")
+            .addEdge("E", "G")
+            .addEdge("C", "F")
+            .addEdge("F", "G");
 
         var expectedTraversalResult = List.of("S", "C", "F", "G", "E", "B", "D", "A");
         assertIterableEquals(expectedTraversalResult, graph.depthFirstTraversal("S"));
@@ -54,18 +56,87 @@ class AdjacencyListGraphTest {
 
     @Test
     void breadthTraversal() {
-        var graph = new AdjacencyListGraph<String>();
-        graph.addEdge("S", "A");
-        graph.addEdge("S", "B");
-        graph.addEdge("S", "C");
-        graph.addEdge("A", "D");
-        graph.addEdge("D", "G");
-        graph.addEdge("B", "E");
-        graph.addEdge("E", "G");
-        graph.addEdge("C", "F");
-        graph.addEdge("F", "G");
+        var graph = new AdjacencyListGraph<String>()
+            .addEdge("S", "A")
+            .addEdge("S", "B")
+            .addEdge("S", "C")
+            .addEdge("A", "D")
+            .addEdge("D", "G")
+            .addEdge("B", "E")
+            .addEdge("E", "G")
+            .addEdge("C", "F")
+            .addEdge("F", "G");
 
         var expectedTraversalResult = List.of("S", "A", "B", "C", "D", "E", "F", "G");
         assertIterableEquals(expectedTraversalResult, graph.breadthFirstSearch("S"));
+    }
+
+    /**
+     * A----B----C-----F
+     * \               \
+     * \               \
+     * D---------E-----G
+     */
+    @Test
+    void shortestPathBFS() {
+        var graph = new AdjacencyListGraph<String>()
+            .addEdge("A", "B")
+            .addEdge("B", "C")
+            .addEdge("C", "E")
+            .addEdge("E", "D")
+            .addEdge("D", "A");
+
+        assertEquals(new Path<>(2, List.of("A", "D", "E")), graph.shortestPathBFS("A", "E"));
+    }
+
+    /**
+     * https://cdncontribute.geeksforgeeks.org/wp-content/uploads/exampleFigure-1.png
+     */
+    @Test
+    void shortestPathBFS1() {
+        var graph = new AdjacencyListGraph<Integer>()
+            .addEdge(2, 1)
+            .addEdge(1, 0)
+            .addEdge(0, 3)
+            .addEdge(3, 4)
+            .addEdge(3, 7)
+            .addEdge(4, 5)
+            .addEdge(4, 7)
+            .addEdge(4, 6)
+            .addEdge(7, 6)
+            .addEdge(5, 6);
+
+        assertEquals(new Path<>(2, List.of(0, 3, 7)), graph.shortestPathBFS(0, 7));
+    }
+
+    @Test
+    void shortestPathBFS2() {
+        var graph = new AdjacencyListGraph<>()
+                .addEdge("A","B")
+                .addEdge("B", "C")
+                .addEdge("C", "D")
+                .addEdge("D", "E")
+                .addEdge("E", "F");
+
+        var expectedPath = new Path<>(5, List.of("A", "B", "C", "D", "E", "F"));
+        assertEquals(expectedPath, graph.shortestPathBFS("A", "F"));
+    }
+
+    @Test
+    void shortestPathBFSNotFound() {
+        var graph = new AdjacencyListGraph<Integer>()
+                .addEdge(2, 1)
+                .addEdge(1, 0)
+                .addEdge(0, 3)
+                .addEdge(3, 4)
+                .addEdge(3, 7)
+                .addEdge(4, 5)
+                .addEdge(4, 7)
+                .addEdge(4, 6)
+                .addEdge(7, 6)
+                .addEdge(5, 6);
+
+        assertEquals(Path.EMPTY, graph.shortestPathBFS(0, 899));
+        assertEquals(Path.EMPTY, new AdjacencyListGraph<>().shortestPathBFS("C", "S"));
     }
 }
